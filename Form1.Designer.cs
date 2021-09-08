e765f61ac33bc19f83f121cb0fd3788c9657338b
mera2002 <SMIS072121@ugb.edu.sv>
2021-09-07 23:40:12 -0600

    genro

---
+++ Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿
+namespace Practica070921
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgData = new System.Windows.Forms.DataGridView();
+            this.btnFill = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgData
+            // 
+            this.dgData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgData.Location = new System.Drawing.Point(267, 127);
+            this.dgData.Name = "dgData";
+            this.dgData.Size = new System.Drawing.Size(502, 175);
+            this.dgData.TabIndex = 0;
+            this.dgData.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // btnFill
+            // 
+            this.btnFill.Location = new System.Drawing.Point(96, 127);
+            this.btnFill.Name = "btnFill";
+            this.btnFill.Size = new System.Drawing.Size(75, 23);
+            this.btnFill.TabIndex = 1;
+            this.btnFill.Text = "GetStudent()";
+            this.btnFill.UseVisualStyleBackColor = true;
+            this.btnFill.Click += new System.EventHandler(this.btnFill_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnFill);
+            this.Controls.Add(this.dgData);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgData)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgData;
+        private System.Windows.Forms.Button btnFill;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practica070921
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void btnFill_Click(object sender, EventArgs e)
+        {
+            dgData.Columns.Add("idStudent", "CODIGO");
+            dgData.Columns.Add("firstName", "NOMBRE");
+            dgData.Columns.Add("lastName", "APELLIDO");
+            dgData.Columns.Add("age", "EDAD");
+            dgData.Columns.Add("gender", "GENERO");
+
+            foreach (var student in selectAllData())
+            {
+                dgData.Rows.Add(student.idStudent, student.firstName, student.lastName,
+                student.age, student.gender);
+
+            }
+        }
+
+            //metodo para retornar la coleccion de datos
+
+            private List<Student> selectAllData()
+        {
+            // crear la consulta 
+            List<Student> result = (
+                   from student in Student.GetStudents()
+                   select new Student()
+                   {
+                       idStudent = student.idStudent,
+                       firstName = student.firstName,
+                       lastName = student.lastName,
+                       age = student.age,
+                       gender = student.gender
+                   }
+
+                   ).ToList();
+
+            return result;
+
+        }
+
+    }
+}
+
